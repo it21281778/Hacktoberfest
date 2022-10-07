148749d4d9c34bc6b97bc6e64a365e124d67e9bd
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-10-07 18:32:48 +0530

    Blink LED

---
+++ Arduino/blink-LED.ino
@@ -0,0 +1,18 @@
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("\n");  
+}
+
+void loop() {
+  Serial.println(i); 
+  digitalWrite(13,HIGH); //light on
+  delay(1500); //increased delay 1.5seconds 
+  digitalWrite(13,LOW); //light off
+  delay(1000);
+  i++;
+  
+}
